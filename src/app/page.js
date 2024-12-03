470e7519fcbcbce10d93146d49e55ee803c929aa
alexissahagun1 <cesar.sahagun@oracle.com>
2024-12-03 11:35:36 -0600

    v2 changes

--- src/app/page.js
+++ src/app/page.js
@@ -17,9 +17,9 @@ export default function MotusExperience() {
             height={100}
             className="mx-auto mb-6"
           />
-          <h1 className="text-5xl font-bold tracking-wider text-pink-500 mb-8 uppercase">Motus Experience</h1>
+          <h1 className="text-5xl font-bold tracking-wider text-pink-500 mb-8 uppercase">Experience</h1>
           
-          <div className="space-y-4 mb-12">
+          <div className="space-y-4 mb-12 uppercase">
             <p className="text-xl">February 28 - March 2, 2025</p>
             <p className="text-xl">Guadalajara, Mexico</p>
           </div>
@@ -32,34 +32,35 @@ export default function MotusExperience() {
           </p>
 
         {/* CTA Section */}
-          <section className="py-16 text-center">
-            <Link href="#pricing" 
-            className="bg-pink-500 hover:bg-pink-600 text-white text-3xl px-8 py-4 rounded">
-              Register Now
-            </Link>
-          </section>
+
         </div>
       </section>
 
 
       {/* About Section */}
-      <section className="py-16">
+      <section className="py-8">
         <div className="container mx-auto px-4 max-w-4xl">
           <p className="text-2xl text-center">
-            Motus is a global dance program designed to elevate artists through intense training.
-            Created by Diana Matos, this program focuses on personal growth, quality of movement,
+            MOTUS is a global dance company designed to train and elevate artists through intense dance training. Created by Diana Matos, this program focuses on personal growth, quality of movement,
             and street styles techniques.
           </p>
         </div>
       </section>
 
       {/* Pricing Section */}
-      <section id='pricing' className="py-16 ">
-  <div className="container mx-auto px-4">
-    <h2 className="text-4xl font-extrabold text-center mb-12 text-pink-500 tracking-wide">
+  <section id='pricing' className="py-16 ">
+    <div className="container mx-auto px-4">
+    {/* <h2 className="text-4xl font-extrabold text-center mb-12 text-pink-500 tracking-wide">
       Pricing Options
-    </h2>
-    <div className="grid md:grid-cols-3 gap-10 max-w-6xl mx-auto perspective-1000">
+    </h2> */}
+    <section className="py-16 text-center">
+      <Link href="" className="bg-pink-500 hover:bg-pink-600 text-white text-3xl px-8 py-4 rounded">
+        Register Now
+      </Link>
+  </section>
+
+  {/* cards */}
+  <div className="grid md:grid-cols-3 gap-10 max-w-6xl mx-auto perspective-1000">
       {/* Card 1 */}
       <div className="group bg-gradient-to-br from-gray-800 to-gray-900 rounded-lg shadow-xl transform transition-transform duration-700 hover:rotate-x-6 hover:rotate-y-6 hover:scale-105 hover:translate-z-10">
         <div className="relative p-6 text-white">
@@ -81,9 +82,7 @@ export default function MotusExperience() {
             </Link>
             </div>
             <div className="p-4 bg-gray-800 rounded-lg shadow-inner hover:bg-gray-700 transition">
-            <Link href='https://buy.stripe.com/test_8wMbKL3is9h18r63ce'>
-              <p className="text-lg font-bold text-gray-300">Motus Members</p>
-            </Link>
+              <p className="text-lg font-bold text-gray-300">price available untill January 15</p>
             </div>
           </div>
         </div>
@@ -109,10 +108,7 @@ export default function MotusExperience() {
             </Link>
             </div>
             <div className="p-4 bg-gray-800 rounded-lg shadow-inner hover:bg-gray-700 transition">
-            <Link href='https://buy.stripe.com/test_8wMbKL3is9h18r63ce'>
-              <p className="text-lg font-bold text-gray-300">Motus Members</p>
-              
-            </Link>
+              <p className="text-lg font-bold text-gray-300">price starting January 15</p>
             </div>
           </div>
         </div>
@@ -122,25 +118,38 @@ export default function MotusExperience() {
       <div className="group bg-gradient-to-br from-gray-800 to-gray-900 rounded-lg shadow-xl transform transition-transform duration-700 hover:rotate-x-6 hover:rotate-y-6 hover:scale-105 hover:translate-z-10">
         <div className="relative p-6 text-white">
           <h3 className="text-3xl font-bold text-pink-500 mb-6 text-center">
-            Special Programs
+            Mentorships
+          </h3>
+          <div className="space-y-4">
+            <div className="p-4 bg-gray-800 rounded-lg shadow-inner hover:bg-gray-700 transition">
+            {/* <Link href='https://buy.stripe.com/test_9AQdST6uEctd5eU9AG'> */}
+              <p className="text-lg font-medium text-gray-300">Adults Diana Matos Mentorship </p>
+              <p className="text-3xl font-bold">$150</p>
+            {/* </Link> */}
+            </div>
+            <div className="p-4 bg-gray-800 rounded-lg shadow-inner hover:bg-gray-700 transition">
+            {/* <Link href='https://buy.stripe.com/test_00g1675qA78T0YE9AH'> */}
+              <p className="text-lg font-medium text-gray-300">Kids Ivan Mentorship </p>
+              <p className="text-3xl font-bold">$100</p>
+            {/* </Link> */}
+            </div>
+            <div className="p-4 bg-gray-800 rounded-lg shadow-inner hover:bg-gray-700 transition">
+              <p className="text-lg font-medium text-gray-300">payment will be processed after audition if selected</p>
+            </div>
+            <p className="text-md font-medium text-gray-300">audition scheduled for the first day of the weekend</p>
+          </div>
+        </div>
+      </div>
+      <div className="group bg-gradient-to-br from-gray-800 to-gray-900 rounded-lg shadow-xl transform transition-transform duration-700 hover:rotate-x-6 hover:rotate-y-6 hover:scale-105 hover:translate-z-10">
+        <div className="relative p-6 text-white">
+          <h3 className="text-3xl font-bold text-pink-500 mb-6 text-center">
+            Showcase
           </h3>
           <div className="space-y-4">
             <div className="p-4 bg-gray-800 rounded-lg shadow-inner hover:bg-gray-700 transition">
             <Link href='https://buy.stripe.com/test_cN28yz06g0KvePu28d'>
-              <p className="text-lg font-medium text-gray-300">Showcase</p>
+              <p className="text-lg font-medium text-gray-300">Register now</p>
               <p className="text-3xl font-bold">$70 per participant</p>
-            </Link>
-            </div>
-            <div className="p-4 bg-gray-800 rounded-lg shadow-inner hover:bg-gray-700 transition">
-            <Link href='https://buy.stripe.com/test_9AQdST6uEctd5eU9AG'>
-              <p className="text-lg font-medium text-gray-300">Adults Mentoring Diana Matos</p>
-              <p className="text-3xl font-bold">$530</p>
-            </Link>
-            </div>
-            <div className="p-4 bg-gray-800 rounded-lg shadow-inner hover:bg-gray-700 transition">
-            <Link href='https://buy.stripe.com/test_00g1675qA78T0YE9AH'>
-              <p className="text-lg font-medium text-gray-300">Kids Mentoring Ivan</p>
-              <p className="text-3xl font-bold">$490</p>
             </Link>
             </div>
           </div>
